1fe58e6016e86fdec7f79f1e15402b9c303f83cf
Joe Kim <joesm.genie@gmail.com>
2024-09-14 11:21:32 +0700

    Implemet basic integration test

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,8 +1,17 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:otp_app_getx/main.dart';
 
 void main() {
-  testWidgets('', (WidgetTester tester) async {
+  testWidgets('Main Application', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
+
+    expect(find.text('0'), findsOneWidget);
+
+    await tester.tap(find.byType(FloatingActionButton));
+    await tester.pump();
+
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
