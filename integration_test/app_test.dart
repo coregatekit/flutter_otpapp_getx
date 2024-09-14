a8ee1a4e128b092e87c28f87c7f1992d6242e9ea
Joe Kim <joesm.genie@gmail.com>
2024-09-14 11:06:41 +0700

    Implement basic UI test

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,1 +1,8 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+import 'package:otp_app_getx/main.dart';
+
+void main() {
+  testWidgets('', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+  });
+}
