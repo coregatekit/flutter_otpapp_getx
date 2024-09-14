27528f3c2eb96a8db25f32c45bfce44cb146249e
Joe Kim <joesm.genie@gmail.com>
2024-09-14 11:57:55 +0700

    Update test for verify success

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:otp_app_getx/main.dart';
+import 'package:otp_app_getx/otp_screen.dart';
 
 void main() {
   testWidgets('Verify Success', (WidgetTester tester) async {
@@ -10,10 +11,14 @@ void main() {
     expect(find.byKey(const Key('phone_number_input')), findsOneWidget);
 
     // Fill valid phone number input
+    await tester.enterText(
+        find.byKey(const Key('phone_number_input')), '0661231111');
 
     // Tap on send otp button
+    await tester.tap(find.byKey(const Key('request_otp_button')));
 
     // Should see otp screen
+    expect(find.byType(OtpScreen), findsOneWidget);
 
     // Fill valid otp code
 
---
+++ lib/otp_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class OtpScreen extends StatelessWidget {
+  const OtpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
